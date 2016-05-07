895c8e78c8551835f5b79a24a0fbb622f37702c7
Storm Tran <bao.trbluecomgroup.com>
2016-05-07 17:26:48 +0700

    refactor api uderscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var bodyparser = require('body-parser');;
+var _ = require('underscore');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -19,15 +20,7 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
 
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if(todoId === todo.id){
-			matchedTodo = todo;
-		}
-		
-	});
-
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 	if(matchedTodo){
 			res.json(matchedTodo);
 		} else {
@@ -36,7 +29,13 @@ app.get('/todos/:id', function (req, res) {
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);
@@ -44,4 +43,4 @@ app.post('/todos', function (req, res) {
 
 app.listen(PORT, function () {
 	console.log('Express is listening on port:' + PORT + '...');
-})
+});
